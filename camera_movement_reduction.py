8574036a8c528d8de72065ef28af1072004ed8b6
Luominai <kevinthepear@gmail.com>
2025-12-09 13:59:26 -0500

    blurring before bg_sub gives better results

--- camera_movement_reduction.py
+++ camera_movement_reduction.py
@@ -1,34 +1,36 @@
 from utils import setup_video_capture
 import cv2 as cv
 
-# video = "videos/Frisco Memorial scrimmage pitching side view - Andy Zappe (1080p, h264).mp4"
+video = "videos/Frisco Memorial scrimmage pitching side view - Andy Zappe (1080p, h264).mp4"
 # video = "videos/Physical Literacy Video Model Throwing (Side View) - Canucks Autism Network Video Models (1080p, h264).mp4"
 # video = "videos/Noah Yoder Face On Side View Vs Hanover HS - Adams Performance Fitness and Physical Therapy (1080p, h264).mp4"
 # video = "videos/Pitching, Side view, June 2025 - Cienna Alvarez (480p, h264).mp4"
-video = "videos\RHP Will Hodges Hanover HS_Liberty Commit) Side View 2nd Inning - Adams Performance Fitness and Physical Therapy (1080p, h264).mp4"
+# video = "videos\RHP Will Hodges Hanover HS_Liberty Commit) Side View 2nd Inning - Adams Performance Fitness and Physical Therapy (1080p, h264).mp4"
+# video = "videos\FRONT ROW AT THE REDS_CARDINALS GAME! - Ethan's Sports Cards & More (720p, h264).mp4"
 
 # Press Q to exit
 # Press S to pause
 # Press D to skip to the next frame
 
 bg_sub = cv.createBackgroundSubtractorKNN()
-bg_sub.setDist2Threshold(3600)
+bg_sub.setDist2Threshold(1600)
 bg_sub.setHistory(10)
 
 def process(img):
     img = cv.resize(img, None, fx=0.34, fy=0.34)
 
-    # apply background subtraction to original image
-    img1 = bg_sub.apply(img)
+    # blur the original image, then apply background subtraction 
+    img1 = cv.blur(img, (3,3))
+    img1 = bg_sub.apply(img1)
     cv.imshow("bg_sub", img1)
 
     # apply canny edge detection to original image
-    img2 = cv.Canny(img, 250, 400)
+    img2 = cv.Canny(img, 200, 400)
     cv.imshow("canny", img2)
 
     # inflate the edges
     img3 = cv.blur(img2, (7,7))
-    ret, img3 = cv.threshold(img3, 0, 200, cv.THRESH_BINARY)
+    ret, img3 = cv.threshold(img3, 30, 200, cv.THRESH_BINARY)
     cv.imshow("blurred canny", img3)
 
     # subtract the inflated edges from the original images to reduce background noise
